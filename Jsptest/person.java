2630722d1b608bd159ccc683344e7ab37899d874
CloverOnly <155341667+CloverOnly@users.noreply.github.com>
2024-05-24 14:45:07 +0900

    Create person.java

---
+++ Jsptest/person.java
@@ -0,0 +1,22 @@
+package webtest;
+
+public class person {
+	private int id=20230821;
+	private String name = "이영진";
+	public person() {}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
